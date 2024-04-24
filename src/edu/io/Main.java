eb337125c924cd54771b5e0f84419dbec7f9b453
timGalk <152897676+timGalk@users.noreply.github.com>
2024-04-24 14:19:43 +0200

    Added new note functionality

---
+++ src/edu/io/Main.java
@@ -0,0 +1,10 @@
+package edu.io;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Note> notes= new ArrayList<>();
+        notes.add(new Note(12, "Git", "Git commands for start: $ git init ... "));
+    }
+}
